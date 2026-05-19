0b78b7e0653d8dd3afd955f4abf831b7f420c181
MatthewGregory421 <136556271+MatthewGregory421@users.noreply.github.com>
2026-05-19 11:30:24 +1000

    started creation of the main  menu and options

---
+++ Assets/Scripts/Managers/MainMenu/MainMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject optionsPanel;
+
+    // Loads the main game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Loads the weekly testing scene
+    public void LoadTestingScene()
+    {
+        SceneManager.LoadScene("PlayerTestingScene");
+    }
+
+    // Opens the options menu
+    public void OpenOptions()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    // Closes the options menu
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // Quits the application
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
