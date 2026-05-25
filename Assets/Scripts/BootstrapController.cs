6c1a37af616b21618f8b02208d0ba3041f10cc71
MatthewGregory421 <136556271+MatthewGregory421@users.noreply.github.com>
2026-05-26 00:29:37 +1000

    bunch of level design
    levels done
    bootstrap
    Tutorial level
    sadness 1
    
    levels started
    sadness 2
    hub
    
    how it works
    - start button loads into bootstrap, teleporters then move the player throughout the levels

--- Assets/Scripts/BootstrapController.cs
+++ Assets/Scripts/BootstrapController.cs
@@ -5,6 +5,6 @@ public class BootstrapController : MonoBehaviour
     private void Start()
     {
         // Start first real gameplay scene
-        SceneTransitionManager.Instance.StartGame("Hub", "Hub_Start");
+        SceneTransitionManager.Instance.StartGame("TutorialScene", "Tutorial_Spawn");
     }
 }
